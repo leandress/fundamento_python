5ddae2b045353aa85a1eb494419828199112affa
leandres <102038431+leandress@users.noreply.github.com>
2022-03-23 20:59:12 +0000

    ejercicio 9

---
+++ ejercicios/ejecicio9.py
@@ -0,0 +1,14 @@
+#generar usuarios de la forma en que se mustra 
+
+nombre=input("ingrese su nombre " )
+apellido=input("ingrese su apellido " )
+edad=input("infrese su edad ")
+
+print("los nombre de usuario")
+
+a=nombre+apellido
+print(a.title())
+b=nombre+"."+apellido
+print(b.lower())
+c=nombre+"_"+apellido+"_"+edad
+print(c.lower())
