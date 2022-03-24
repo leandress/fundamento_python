c36898acdd26473248c11f96b99c13884cffb8df
leandres <102038431+leandress@users.noreply.github.com>
2022-03-24 14:46:59 +0000

    cadenas

---
+++ ejercicios/strings.py
@@ -0,0 +1,25 @@
+texto="Leo es la para del movimiento  "
+parrafo="Leo sigue siendo el rey..... "
+
+#operaciones 
+print(texto + parrafo)
+
+#print(texto - parrafo) error
+
+#print(texto * parrafo) error
+
+#print(texto / parrafo) error
+
+#operaciones con string y numeros
+
+#print(texto + 5) error
+
+
+#print(texto - 5) error
+
+
+print(texto * 5)
+
+
+#print(texto / 5) error
+
