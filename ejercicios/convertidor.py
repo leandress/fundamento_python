2d703ac54b8fd3502ef0ee03c3645b9cc777f280
leandres <102038431+leandress@users.noreply.github.com>
2022-03-24 15:17:34 +0000

    converit

---
+++ ejercicios/convertidor.py
@@ -0,0 +1,7 @@
+#hacer un program que me pida digitar un monto en pesos y mostar el resultado en dolares.hacerlo user-friendly
+
+print("saludos.\nporfavor ingrese la cantidad que desea cambiar")
+pesos=float(input())
+dolar="{0:.2f}".format(pesos/55.15)
+
+print("el monto introducido= ",pesos, "convertio a peso es= ", dolar,"gracias por preferirnos")
