914ad4e05e2e9391830226b4825350947fcd13d1
leandres <102038431+leandress@users.noreply.github.com>
2022-03-30 14:37:47 +0000

    lista_tuplas

---
+++ ejercicios/tipos_iterables.py
@@ -0,0 +1,9 @@
+#listas 
+#apend() remove() pop() reverse() count() insert() index() extend()
+dias= "lunes", "martes", "miercoles"
+
+dias.append("jueves")
+dias.insert(0,"domingo")
+
+#tuplas
+meses=("enero",)
