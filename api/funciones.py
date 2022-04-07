77131f36cb67791ba3da8480af7776686830cd18
leandres <102038431+leandress@users.noreply.github.com>
2022-04-07 15:53:53 +0000

    funciones

---
+++ api/funciones.py
@@ -0,0 +1,21 @@
+import requests
+
+def verlistado()url="url por defecto":
+  print("Listado de pokemones")
+  if respuesta:
+    datos = respuesta.json()
+
+    primero=None
+    ultimo=None
+
+    for pokemon in datos ["results"]:
+      id_pokemon=pokemon["url"]
+def verpokemon():
+  print("ver pokemon")
+def buscarpokemonporid():
+  print("buscandi id")
+def buscarpokemonporsunombre():
+  print("buscando pokemon")
+def salir():
+  print ("bye")
+
